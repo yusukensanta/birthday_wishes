ff3ed4694da0bdb2e087a696fe9d5abc340bffe6
yusukensanta <yusuke.nakayama1218@gmail.com>
2024-05-02 02:13:55 +0900

    feat: add sub command with parameters

--- main.py
+++ main.py
@@ -9,6 +9,36 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 intents = discord.Intents.none()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/", case_insensitive=True, intents=intents)
+group = app_commands.Group(name="bd", description="birthday commands")
+bot.tree.add_command(group)
+
+
+@group.command(description="誕生日を登録する")
+@app_commands.describe(month="月", day="日", user="対象のユーザー")
+async def reg(
+    interaction: discord.Interaction,
+    month: int = 1,
+    day: int = 1,
+    user: discord.Member = None,
+):
+    # TODO: dbへ登録
+    await interaction.response.send_message(f"{month}/{day}", ephemeral=True)
+
+
+@group.command(description="現在登録されている誕生日リストを見る")
+async def ls(interaction: discord.Interaction):
+    # TODO: dbから一覧取得
+    await interaction.response.send_message(
+        "登録されている誕生日リスト\nhoge\nhoge\nhoge", ephemeral=True
+    )
+
+
+@group.command(description="誕生日登録を消す")
+@app_commands.describe(user="消す対象の人")
+async def rm(interaction: discord.Interaction, user: discord.Member):
+    await interaction.response.send_message(
+        f"{user.display_name}の誕生日を消しました", ephemeral=True
+    )
 
 
 @bot.event
@@ -19,29 +49,6 @@ async def on_ready():
         print(f"Synced {len(synced)} commands")
     except Exception as e:
         print(e)
-
-
-@bot.tree.command(name="reg")
-@app_commands.describe(month="月", day="日")
-async def reg(interaction: discord.Interaction, month: int = 1, day: int = 1):
-    # TODO: dbへ登録
-    await interaction.response.send_message(f"{month}/{day}", ephemeral=True)
-
-
-@bot.tree.command(name="ls")
-async def ls(interaction: discord.Interaction):
-    # TODO: dbから一覧取得
-    await interaction.response.send_message(
-        "登録されている誕生日リスト\nhoge\nhoge\nhoge", ephemeral=True
-    )
-
-
-@bot.tree.command(name="rm")
-@app_commands.describe(user="消す対象の人")
-async def rm(interaction: discord.Interaction, user: discord.Member):
-    await interaction.response.send_message(
-        f"{user.name}の誕生日を消しました", ephemeral=True
-    )
 
 
 bot.run(TOKEN)
