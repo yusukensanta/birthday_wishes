5deb92a51265955b3ab6f8db717c21ca9d9bbb51
yusukensanta <yusuke.nakayama1218@gmail.com>
2024-05-04 13:51:16 +0900

    feat: add background task to send birthday wishes at 00:00:00 jst

--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,13 @@
-from datetime import time
+from datetime import datetime, time
 from zoneinfo import ZoneInfo
 
 from discord.ext import commands, tasks
 
-scheduled_time = time(hour=0, minute=0, tzinfo=ZoneInfo("Asia/Tokyo"))
+from src.table_manager import BirthdayManager, ChannelManager
+
+scheduled_time = time(
+    hour=0, minute=0, second=0, tzinfo=ZoneInfo("Asia/Tokyo")
+)
 
 
 class BirthdayBot(commands.Bot):
@@ -12,7 +16,29 @@ class BirthdayBot(commands.Bot):
 
     @tasks.loop(time=scheduled_time)
     async def birthday_wish(self):
-        print("Wishing happy birthday to everyone")
+        today = datetime.now(ZoneInfo("Asia/Tokyo"))
+        for guild in self.guilds():
+            server_id = guild.id
+            birthday_manager = BirthdayManager(server_id=server_id)
+            birthdays = birthday_manager.list_all()
+            if not birthdays:
+                continue
+
+            else:
+                for birthday in birthdays:
+                    if (
+                        birthday["birth_month"] == today.month
+                        and birthday["birth_day"] == today.day
+                    ):
+                        channel_manager = ChannelManager(server_id=server_id)
+                        channel = channel_manager.list_all(
+                            server_id=server_id
+                        )[0]
+                        if not channel:
+                            continue
+                        await self.get_channel(channel["channel_id"]).send(
+                            "Happy Birthday!"
+                        )
 
     @birthday_wish.before_loop
     async def before_birthday_wish(self):
